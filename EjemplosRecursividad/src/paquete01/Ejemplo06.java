83b114f8f3a4a1731e7688e298e580147632295a
Denis Ruiz <denisruiz112789@gmail.com>
2023-01-11 10:45:36 -0500

    SEMANA 5

--- EjemplosRecursividad/src/paquete01/Ejemplo06.java
+++ EjemplosRecursividad/src/paquete01/Ejemplo06.java
@@ -4,26 +4,53 @@
  */
 package paquete01;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Ejemplo06 {
 
     public static void main(String[] args) {
-        
-        int valor1 = 5; // 3 // 6
-        int valor2 = 4; // 5 // 1
-        
-        System.out.printf("%d\n", obtenerSuma(valor1, valor2));
+        String [] arreglo = obtenerArreglo();
+        System.out.println("------------------------------------");
+        System.out.println("\nEl arreglo:");
+        for(int i = 0; i < arreglo.length; i++){
+            System.out.printf("[%s] ", arreglo[i]);
+        }
+        System.out.println("\nDebe tener valores de 4 o 5:");
+        obtenerValores(arreglo);
     }
-
-    public static int obtenerSuma(int a, int b) {
-        if (b == 0) {
-            return a;
-        } else {
-            if (a == 0) {
-                return b;
-            } else {
-                return 1 + obtenerSuma(a, b - 1);
+    
+    public static String [] obtenerArreglo(){
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        System.out.println("Ingrese el numero de elementos que contendra "
+                + "el arreglo:");
+        int elementos = entrada.nextInt();
+        entrada.nextLine();
+        String [] cadena = new String [elementos];
+        for (int i = 0; i < cadena.length; i++) {
+            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            System.out.println("Ingrese Ciudades del Ecuador: ");
+            String ciudad = entrada.nextLine();
+            cadena[i] = ciudad;
+        }
+        String mensaje = "";
+        for (int i = 0; i < cadena.length; i++) {
+            mensaje = String.format("%s%s",mensaje, cadena[i]);
+        }
+        return cadena;
+    }
+    
+    public static void obtenerValores(String [] arreglo){
+        int datos;
+        for(int i = 0; i < arreglo.length; i++){
+            datos = arreglo[i].length();
+            if(datos == 4 || datos == 5){
+                System.out.printf("%s\n", arreglo[i]);
             }
         }
     }
-
-}
+}  
+//INTEGRANTES
+//Denis Ruiz
+//Steveen Cuenca
--- EjemplosRecursividad/src/paquete02/Ejemplo08.java
+++ EjemplosRecursividad/src/paquete02/Ejemplo08.java
@@ -4,9 +4,27 @@
  */
 package paquete02;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Ejemplo08 {
 
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in); 
+        entrada.useLocale(Locale.US);
+        System.out.println("Ingrese el limite del arreglo");
+        int tamaño = entrada.nextInt();
+        entrada.nextLine();
+        int [] arreglo = new int [tamaño];
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.printf("Ingrese el valor para la posicion %d\n", i);
+            int dato = entrada.nextInt();
+            arreglo[i]=dato;
+        }
+        int resultado;
+        resultado = misterio(arreglo, tamaño);
+        System.out.printf("El resultado es %s\n",resultado);
+        
         // Ingresar por teclado el tamaño de un arreglo
         // Ingresar por teclado los valores enteros del arreglo
         // Sumar los valores del arreglo, mediante la función misterio
